a47e1374b5f167781846b67230b1ad6cfd8eb03e
Marjan Magharehi <marjan.magharehi@gmail.com>
2019-10-16 15:08:27 +0100

    Add notes to directory

--- directory.rb
+++ directory.rb
@@ -12,7 +12,7 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
-# finally, we print the total number of students
+#finally, we print the total number of students
 print "Overall, we have "
 #it's important that print() doesn't add new line characters
 print 11
